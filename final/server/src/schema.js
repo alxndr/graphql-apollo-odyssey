33d0f4f47b6c12f1d568297564217f4c1395b672
Alexander <alxndr+github@gmail.com>
2023-03-24 10:06:33 -0700

    next thing

--- final/server/src/schema.js
+++ final/server/src/schema.js
@@ -1,6 +1,9 @@
 const { gql } = require('apollo-server');
 
 const typeDefs = gql`
+  # in gql, a type with a ! means "...and never a null"; without a ! means "...or Maybe a null"
+
+  "…the top-level fields that our client can query…"
   type Query {
     "Query to get tracks array for the homepage grid"
     tracksForHome: [Track!]!
@@ -9,14 +12,19 @@ const typeDefs = gql`
   "A track is a group of Modules that teaches about a specific topic"
   type Track {
     id: ID!
+
     "The track's title"
     title: String!
+
     "The track's main Author"
     author: Author!
+
     "The track's illustration to display in track card or track page detail"
     thumbnail: String
+
     "The track's approximate length to complete, in minutes"
     length: Int
+
     "The number of modules this track contains"
     modulesCount: Int
   }
@@ -24,8 +32,10 @@ const typeDefs = gql`
   "Author of a complete Track or a Module"
   type Author {
     id: ID!
+
     "Author's first and last name"
     name: String!
+
     "Author's profile picture"
     photo: String
   }
